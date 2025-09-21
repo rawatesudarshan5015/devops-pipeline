b1dab22a9e58a46a6255d36101e8899dc470e4e9
Sudarshan Rawate <rawatesudarshan5015@gmail.com>
2025-09-21 12:06:12 +0530

    feature: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("hello Devops");
+console.log("I'm learning CI/CD pipeline using github actions");
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("starTing the testing....");
+setTimeout(() => {
+  console.log("test completed");
+}, 3000);
+
+console.log("Completed the testing....");
